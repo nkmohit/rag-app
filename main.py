f5c06187ab7c132d0c49a4b1c0d03a3ca5d6aed1
nkmohit <nmsk3219@gmail.com>
2026-02-01 13:57:02 +0530

    0.1 upload file

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import os
 
 app = FastAPI()
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 @app.get("/")
 def read_root():
@@ -10,3 +14,22 @@ def read_root():
 def health():
     return {"status": "healthy"}
 
+@app.post("/upload")
+async def upload_file(file : UploadFile):
+    """
+    Accept a file and save it locally.
+    STEP 1 of Ingestion pipeline.
+    
+    :param file: File to be embeded
+    :type file: UploadFile
+    """
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+    with open(file_path, "wb") as f:
+        content = await file.read()
+        f.write(content)
+    
+    return {
+        "filename" : file.filename,
+        "status": "saved"
+    }
